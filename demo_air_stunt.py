a3885f2fe53388554b63e4e77b5dbcc335e003aa
Yakunin Gleb <yakuningleb6@gmail.com>
2026-08-18 16:52:35 +0300

    trailing whitespace removed

--- demo_air_stunt.py
+++ demo_air_stunt.py
@@ -56,7 +56,7 @@ def main() -> None:
         drone = sim.spawn("F450", x=SPAWN[0], y=SPAWN[1], z=SPAWN[2])
         hx, hy, hz = xyz(sim, drone.instance_id)
         z = hz + CRUISE_Z
-   
+
         sim.start()
         deadline = time.time() + 5
         while time.time() < deadline:
